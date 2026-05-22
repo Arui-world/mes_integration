924ba197bdd79ae9215287c034238e6e9ab5c69c
cr <308642281@qq.com>
2026-05-22 02:23:51 +0000

    update

--- mes_integration/public/js/material_request.js
+++ mes_integration/public/js/material_request.js
@@ -1,3 +1,9 @@
+const INJECTION_MOLDING_PURPOSE = "Injection Molding Issuance";
+const INJECTION_MOLDING_WEIGHT_FIELDS = [
+	"custom_new_material_weight",
+	"custom_recycled_material_weight"
+];
+
 frappe.ui.form.on("Material Request", {
 	onload: function(frm) {
 		toggle_injection_molding_weight_fields(frm);
@@ -31,10 +37,11 @@ function toggle_injection_molding_weight_fields(frm) {
 
 	ensure_material_request_item_grid_defaults(grid);
 
-	if (frm.doc.material_request_type === "Injection Molding Issuance") {
+	if (frm.doc.material_request_type === INJECTION_MOLDING_PURPOSE) {
 		apply_injection_molding_item_grid(grid);
 	} else {
 		restore_material_request_item_grid(grid);
+		hide_injection_molding_weight_fields(grid);
 	}
 
 	rebuild_material_request_item_grid(grid);
@@ -43,32 +50,25 @@ function toggle_injection_molding_weight_fields(frm) {
 function apply_injection_molding_item_grid(grid) {
 	const injection_columns = [
 		["item_code", 2],
-		["schedule_date", 1],
+		["schedule_date", 2],
 		["qty", 1],
-		["warehouse", 1],
+		["warehouse", 2],
 		["uom", 1],
-		["custom_new_material_weight", 2],
-		["custom_recycled_material_weight", 2]
+		["projected_qty", 1],
+		["custom_new_material_weight", 1],
+		["custom_recycled_material_weight", 1]
 	];
 	const injection_fieldnames = injection_columns.map(function(column) {
 		return column[0];
 	});
 
 	grid.docfields.forEach(function(df) {
-		if (!df.fieldname) {
+		if (!df.fieldname || !injection_fieldnames.includes(df.fieldname)) {
 			return;
 		}
 
-		if (df.fieldname === "projected_qty") {
-			df.hidden = 1;
-			df.in_list_view = 0;
-			return;
-		}
-
-		if (injection_fieldnames.includes(df.fieldname)) {
-			df.hidden = 0;
-			df.in_list_view = 1;
-		}
+		set_material_request_item_docfield_property(grid, df.fieldname, "hidden", 0);
+		set_material_request_item_docfield_property(grid, df.fieldname, "in_list_view", 1);
 	});
 
 	grid.visible_columns = injection_columns
@@ -81,8 +81,8 @@ function apply_injection_molding_item_grid(grid) {
 				return null;
 			}
 
-			df.columns = column[1];
-			df.colsize = column[1];
+			set_material_request_item_docfield_property(grid, df.fieldname, "columns", column[1]);
+			set_material_request_item_docfield_property(grid, df.fieldname, "colsize", column[1]);
 
 			return [df, column[1]];
 		})
@@ -98,14 +98,23 @@ function restore_material_request_item_grid(grid) {
 			return;
 		}
 
-		df.hidden = defaults.hidden;
-		df.in_list_view = defaults.in_list_view;
-		df.columns = defaults.columns;
-		df.colsize = defaults.colsize;
+		set_material_request_item_docfield_property(grid, df.fieldname, "hidden", defaults.hidden);
+		set_material_request_item_docfield_property(grid, df.fieldname, "in_list_view", defaults.in_list_view);
+		set_material_request_item_docfield_property(grid, df.fieldname, "columns", defaults.columns);
+		set_material_request_item_docfield_property(grid, df.fieldname, "colsize", defaults.colsize);
 	});
 
 	grid.visible_columns = [];
 	grid.user_defined_columns = [];
+}
+
+function hide_injection_molding_weight_fields(grid) {
+	INJECTION_MOLDING_WEIGHT_FIELDS.forEach(function(fieldname) {
+		set_material_request_item_docfield_property(grid, fieldname, "hidden", 1);
+		set_material_request_item_docfield_property(grid, fieldname, "in_list_view", 0);
+		set_material_request_item_docfield_property(grid, fieldname, "columns", undefined);
+		set_material_request_item_docfield_property(grid, fieldname, "colsize", undefined);
+	});
 }
 
 function ensure_material_request_item_grid_defaults(grid) {
@@ -126,6 +135,32 @@ function ensure_material_request_item_grid_defaults(grid) {
 			columns: df.columns,
 			colsize: df.colsize
 		};
+	});
+}
+
+function set_material_request_item_docfield_property(grid, fieldname, property, value) {
+	const grid_docfield = grid.docfields.find(function(df) {
+		return df.fieldname === fieldname;
+	});
+
+	if (grid_docfield) {
+		grid_docfield[property] = value;
+	}
+
+	const meta_docfield = frappe.meta.get_docfield("Material Request Item", fieldname);
+
+	if (meta_docfield) {
+		meta_docfield[property] = value;
+	}
+
+	(grid.grid_rows || []).forEach(function(row) {
+		const row_docfield = row.docfields && row.docfields.find(function(df) {
+			return df.fieldname === fieldname;
+		});
+
+		if (row_docfield) {
+			row_docfield[property] = value;
+		}
 	});
 }
 
